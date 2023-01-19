894cbfee6bbf6cc9c556f32e4069653d48fa4a5a
Lucas Comamala <lucascomamala@gmail.com>
2023-01-19 15:21:30 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,3 +13,23 @@ CREATE TABLE animals (
 
 /* Day 2 */
 ALTER TABLE animals ADD species VARCHAR(50);
+
+/* Day 3 */
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
