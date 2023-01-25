17f9e1b5c7334477c310455c3cee075721efe075
Raymond Olisa <raymondolisa01@gmail.com>
2023-01-25 21:38:57 +0100

    Create index for animal_id, vet_id and email

--- schema.sql
+++ schema.sql
@@ -59,3 +59,7 @@ CREATE TABLE visits(
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX idx_visits_animal_id ON visits(animal_id);
+CREATE INDEX idx_visits_vet_id ON visits(vet_id);
+CREATE INDEX idx_owners_email ON owners(email);
